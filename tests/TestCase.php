5dc47cf6c44e1ab480df14486899fec60038eea3
greeflas <vldmr.kuprienko@gmail.com>
2018-12-02 19:16:42 +0200

    Creates tests

---
+++ tests/TestCase.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Greeflas\Store\Tests;
+
+abstract class TestCase
+{
+    public static function run()
+    {
+        $test = new static();
+        $test->testSet();
+        $test->testGet();
+        $test->testHas();
+        $test->testRemove();
+        $test->testClear();
+    }
+
+    abstract public function testSet();
+    abstract public function testGet();
+    abstract public function testHas();
+    abstract public function testRemove();
+    abstract public function testClear();
+
+    protected function assertEquals($expected, $actual)
+    {
+        if ($actual === $expected) {
+            echo \sprintf("%s is OK!\n", static::class);
+
+            return;
+        }
+
+        echo \sprintf(
+            "%s failed assert that '%s' equals '%s'\n",
+            static::class,
+            \var_export($expected, true),
+            \var_export($actual, true)
+        );
+    }
+}
